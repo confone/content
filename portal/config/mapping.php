03b115b7b32d4a46ac8bcb560df1c19b06f74b53
peng.shen <peng.shen@indochino.com>
2014-04-15 15:39:36 -0700

    update config for new pages

--- portal/config/mapping.php
+++ portal/config/mapping.php
@@ -1,5 +1,12 @@
 <?php
 register('/index', new ContentHomeController());
-register('/image', new ImageDetailController());
-register('/text', new TextDetailController());
+
+register('/project/list', new ProjectListController());
+register('/project/detail', new ProjectDetailController());
+register('/project/path', new PathDetailController());
+
+register('/image/detail', new ImageDetailController());
+register('/text/detail', new TextDetailController());
+
+register('/howto', new ContentHowtoController());
 ?>
